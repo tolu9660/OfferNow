973662064bd36233055930864e8ec9852a1b97a4
tolu9660 <57481524+tolu9660@users.noreply.github.com>
2021-04-04 11:15:18 +0200

    nuevo

---
+++ proyecto/practica2/cabecera.php
@@ -0,0 +1,43 @@
+<header >
+	<div id= "titulo">
+	<img src="imagenes/OfferNow_Logo.ico"  ALIGN=DOWN alt="OfferNowLogo"/>
+	
+	<h1>
+	Tu página de ofertas preferida
+	</h1>
+	</div>
+	<div id="contenedor1">
+		<div class="menu" class="col-4 my-auto mx-auto">
+			<ul>
+				<li><a href="#">Destacados</a></li>
+				<li><a href="#">Todos</a></li>
+				<li><a href="#">Nuestra tienda</a></li>
+				
+			</ul>
+		</div>
+		<div class="sesion">
+			<ul>
+				<li><a href="#">Inicio Sesión</a></li>
+				<li><a href="#">Registro</a></li>
+				<li><a href="#">Hazte premium</a></li>
+			</ul>
+		</div>
+		<div class="carrito">
+			<!-- preguntar como darle funcionalidad al boton, ES CORRRECTO ?-->
+			<button class="button">    
+                <img src="imagenes/iconos/carrito.png" width="30" height="30" alt="votos"/>    
+            </button>
+
+			<a href="carrito.php" rel="nofollow" target="_blank" class="button" >
+			<img src="imagenes/iconos/carrito.png" width="40" height="40" alt="votos"/>    
+
+			</a>
+		</div>
+
+	</div>
+
+	
+
+
+</header>
+		
---
+++ proyecto/practica2/movil.php
@@ -0,0 +1,54 @@
+
+	   
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="estilo.css" />
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+
+        <link rel="stylesheet" href="/css/styles.css?v=<?php echo time(); ?>" />
+        <script src="/js/functions.js?v=<?php echo time(); ?>"></script>
+
+
+        <title>movil</title>
+    </head>
+
+    <body>
+        <?php
+                    require('cabecera.php')	
+        ?>
+        <div id="tarjetaProducto">
+            <button class="button" type="button">    
+                <img src="imagenes/iconos/ok.png" width="15" height="15" alt="votos"/>    
+                VOTOS: 1222
+
+            </button>
+           <div class="imgProducto">
+                <img src="imagenes/productos/movil.png" width="200" height="200" alt="movil" />
+           </div>
+            <div class="desProducto">
+                <p>
+                    Descripción de la oferta 
+                </p>
+                <p>
+                    Enlaces: 
+                    <a href="/enlace-producto-2" rel="nofollow" target="_blank" >Enlace Oferta</a> /
+                    <a href="/enlace-producto-2" rel="nofollow" target="_blank">Enlace a nuestra tienda</a>
+                </p>
+            </div>
+            <div class="desProducto">
+                <p>
+                    Comentarios:
+                </p>    
+                <p>
+                    <textarea name="Comentarios" placeholder= "Escribe tu comentario.." 
+                                    rows="2" cols="30"></textarea>
+                </p>
+                <input type="button" value="publicar">
+            </div>              
+            
+        </div>
+    
+
+    </body>
+</html>

381ce61f247c7df684dc5f2a9fc6a76a3a34e679
PabloSaenz99 <60698400+PabloSaenz99@users.noreply.github.com>
2021-04-13 16:28:57 +0200

    Comentarios en productos

--- proyecto/practica2/producto.php
+++ proyecto/practica2/producto.php
@@ -28,27 +28,42 @@
 	$comentarios = $ofertaObj->muestraComentarios();
 
 	//Muestra las cosas
-	$contenidoPrincipal=<<<EOS
-        <div id="tarjetaProducto">
-            <button class="button" type="button">    
-                <img src="imagenes/iconos/ok.png" width="15" height="15" alt="votos"/>    
-                VOTOS: $Valoracion
-
-            </button>
-           <div class="imgProducto">
-                <img src="$URLImagen" width="200" height="200" alt=$Nombre />
-           </div>
-            <div class="desProducto">
+	$productos = '';
+	$productos.=<<<EOS
+		<div id="tarjetaProducto">
+			<button class="button" type="button">    
+				<img src="imagenes/iconos/ok.png" width="15" height="15" alt="votos"/>    
+				VOTOS: $Valoracion
+			</button>
+			<div class="imgProducto">
+				<img src="$URLImagen" width="200" height="200" alt=$Nombre />
+			</div>
+			<div class="desProducto">
 				<p>Nombre del producto:</p>
-                <p>$Nombre</p>
+				<p>$Nombre</p>
 				<p>Descripcion:</p>
-                <p>$Descripcion</p>
-                <p>
-                    Enlaces: 
-                    <a href="https://$URLOferta" rel="nofollow" target="_blank" >Enlace Oferta</a> /
-                    <a href="$URLOferta" rel="nofollow" target="_blank">Enlace a nuestra tienda</a>
-                </p>
-            </div>
+				<p>$Descripcion</p>
+				<p>
+					Enlaces: 
+						<a href="https://$URLOferta" rel="nofollow" target="_blank" >Enlace Oferta</a> /
+						<a href="$URLOferta" rel="nofollow" target="_blank">Enlace a nuestra tienda</a>
+				</p>
+			</div>
+	EOS;
+	for($i = 0; $i < sizeof($comentarios); $i++){
+		$comTitulo = $comentarios[$i]->muestraTitulo();
+		$comTexto = $comentarios[$i]->muestraTexto();
+		$comValoracion = $comentarios[$i]->muestraValoracion();
+		$comUsuario = $comentarios[$i]->muestraUsuario();
+		$productos.=<<<EOS
+			<div class="comProducto">
+				<p>$comTitulo - $comUsuario - </p>
+				<p>Valoración comentario: $comValoracion</p>
+				<p>$comTexto</p>
+			</div>
+		EOS;
+	}
+	$productos.=<<<EOS
             <div class="creaComentario">
 			
 				<h1>Subir Comentario</h1>
@@ -56,7 +71,7 @@
 					<p>Titulo</p>
 					<input type="text" name="comentarioTitulo"/>
 					<p>Descripcion:</p>
-					<textarea name="comentarioDescripcion" rows="10" cols="30"></textarea>
+					<textarea name="comentarioDescripcion" rows="5" cols="48"></textarea>
 					<input type="hidden" value="$id" name="comentarioUrlDeOferta"/>
 					<p><input type="submit" value="Publicar"></p>
 					
@@ -64,5 +79,9 @@
             </div>              
         </div>
 EOS;
+
+$contenidoPrincipal=<<<EOS
+		$productos
+	EOS;
 
 require __DIR__.'/includes/comun/layout.php';

2a92900aaeae75809858ee7c166b7233b5af1ad4
tolu9660 <57481524+tolu9660@users.noreply.github.com>
2021-04-13 11:19:26 +0200

    Update inicio.php

--- proyecto/practica2/inicio.php
+++ proyecto/practica2/inicio.php
@@ -34,59 +34,53 @@ else{
 
 
 $tituloPagina = 'Inicio';
+/*
 //habria que automatizar este proceso
 $url=strval($ofertasArray[2]->muestraURLOferta());
 $url1=strval($ofertasArray[2]->muestraURLImagen());
+*/
 
+$productos;
+for ($i = 0; $i < $result->num_rows; $i++) {
+	$url=strval($ofertasArray[$i]->muestraURLOferta());
+	$url1=strval($ofertasArray[$i]->muestraURLImagen());
+	//echo $url."<br />";
+	//echo $url1;
+	$productos.=<<<EOS
+	<li>
+		<a href=$url rel="nofollow" target="_blank">
+		<img src=$url1 width="200" height="200" alt="movil" />
+		<h3>Movil Barato</h3>
+		<p>14.39 €</p>
+		</a>
+	</li>	
+EOS;
+}
 $contenidoPrincipal=<<<EOS
+	$productos;
+EOS;
+
+/*$contenidoPrincipal=<<<EOS
 	<div="contenedor">
-			<?php
-					//require('cabecera.php');
-			?>
-			
+				
 			<ul class="rejilla">
 			<!--buscar la manera de automatizar este proceso -->
 			<!-- otra manera es ir creando con clases dentro de otras, la gestion se haría con clases anidadas -->
-			  <li>
-				<a href=$url rel="nofollow" target="_blank">
-				<img src=$url1 width="200" height="200" alt="movil" />
-				<h3>Movil Barato</h3>
-				<p>14.39 €</p>
-				</a>
-			  </li>
+  			
+			
+					
+				<li>
+					<a href=$url rel="nofollow" target="_blank">
+					<img src=$url1 width="200" height="200" alt="movil" />
+					<h3>Movil Barato</h3>
+					<p>14.39 €</p>
+					</a>
+				</li>
+			
+		
 
-			  <li>
-				<a href="/enlace-producto-2" rel="nofollow" target="_blank">
-				  <img src="imagenes/productos/ram.png" width="200" height="200" alt="ram" />
-				  <h3>Nombre de ejemplo 2</h3>
-				  <p>37.95 €</p>
-				</a>
-			  </li>
-
-			  <li>
-				<a href="/enlace-producto-2" rel="nofollow" target="_blank">
-				  <img src="imagenes/productos/nevera.png" width="200" height="200" alt="ram" />
-				  <h3>Nombre de ejemplo 2</h3>
-				  <p>37.95 €</p>
-				</a>
-			  </li>
-			  <li>
-				<a href="/enlace-producto-2" rel="nofollow" target="_blank">
-				  <img src="imagenes/productos/ram.png" width="200" height="200" alt="ram" />
-				  <h3>Nombre de ejemplo 2</h3>
-				  <p>37.95 €</p>
-				</a>
-			  </li>
-
-			  <li>
-				<a href="/enlace-producto-2" rel="nofollow" target="_blank">
-				  <img src="imagenes/productos/nevera.png" width="200" height="200" alt="ram" />
-				  <h3>Nombre de ejemplo 2</h3>
-				  <p>37.95 €</p>
-				</a>
-			  </li>
 			</ul>
 		</div>
-EOS;
+EOS;*/
 
 require __DIR__.'/includes/comun/layout.php';

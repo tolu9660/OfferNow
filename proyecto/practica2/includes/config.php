f7121d2216e54deef3cc1cd14b3a12f623a81e39
tolu9660 <57481524+tolu9660@users.noreply.github.com>
2021-04-29 14:27:59 +0200

    Create config.php

---
+++ proyecto/practica2/includes/config.php
@@ -0,0 +1,77 @@
+<?php
+
+// Varios defines para los parámetros de configuración de acceso a la BD y la URL desde la que se sirve la aplicación
+define('BD_HOST', 'localhost');
+define('BD_NAME', 'aw_p2');
+define('BD_USER', 'root');
+define('BD_PASS', '');
+define('RUTA_APP', '/proyecto/practica2');					//Ruta de Luis
+//define('RUTA_APP', '/AW/OfferNow/proyecto/practica2');	//Ruta de Pablo
+define('RUTA_IMGS', RUTA_APP.'/imagenes');
+define('RUTA_CSS', RUTA_APP.'/css');
+define('COMUN', RUTA_APP.'/includes/comun');
+define('INSTALADA', true );
+
+
+if (! INSTALADA) {
+    echo "La aplicación no está configurada";
+    exit();
+  }
+
+
+/* */
+/* Configuración de Codificación */
+/* */
+
+ini_set('default_charset', 'UTF-8');
+setLocale(LC_ALL, 'es_ES.UTF.8');
+
+/* */
+/* Funciones de gestión de la conexión a la BD */
+/* */
+
+$BD = null;
+function getConexionBD() {
+    global $BD;
+    if (!$BD) {
+      $BD = new mysqli(BD_HOST, BD_USER, BD_PASS, BD_NAME);
+      if ( $BD->connect_errno ) {
+        echo "Error de conexión a la BD: (" . $BD->connect_errno . ") " . $BD->connect_error;
+        exit();
+      }
+      if ( ! $BD->set_charset("utf8mb4")) {
+        echo "Error al configurar la codificación de la BD: (" . $BD->errno . ") " . $BD->error;
+        exit();
+      }
+    }
+    return $BD;
+  }
+
+function cierraConexion() {
+    // Sólo hacer uso de global para cerrar la conexion !!
+    global $BD;
+    if ( isset($BD) && ! $BD->connect_errno ) {
+      $BD->close();
+    }
+  }
+
+register_shutdown_function('cierraConexion');
+  
+  
+  /* */
+  /* Usuario almacenado en memoria */
+  /* */
+  
+  /* XXX Quita este comentario y comenta UsuarioBD.php para proba SOLO 
+  la funcionalidad de login del usuario.
+  require_once __DIR__.'/UsuarioArray.php';
+  */
+  
+  /* */
+  /* Usuario almacenado en BD */
+  /* */
+  
+  //require_once __DIR__.'/UsuarioBD.php';
+  
+  // Abrimos la sesión por defecto en todas las peticiones
+  session_start();

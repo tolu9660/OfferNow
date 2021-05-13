fa1e1a044ae150f48a450c5a1a159b0fc4527e7e
tolu9660 <57481524+tolu9660@users.noreply.github.com>
2021-05-13 12:40:01 +0200

    Add files via upload

--- includes/mysql/aw_p2.sql
+++ includes/mysql/aw_p2.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 13-05-2021 a las 12:25:50
+-- Tiempo de generación: 13-05-2021 a las 12:39:30
 -- Versión del servidor: 10.4.17-MariaDB
 -- Versión de PHP: 7.4.15
 
@@ -47,7 +47,7 @@ INSERT INTO `articulos_segunda_mano` (`Numero`, `Nombre`, `Descripcion`, `Unidad
 (10, 'Pesas_2kg', 'Set de 2 pesas de vinilo de 2 kg perfectas para sus entrenamientos habituales.', 3, 15, 'Pesas.jpg', 0),
 (11, 'Redmi_Note_8T', 'El Xiaomi Redmi Note 8T es un smartphone Android con una pantalla Full HD+ de 6.3 pulgadas y potenciado por un procesador Snapdragon 665 de ocho núcleos, acompañado de 3GB de memoria RAM con 32GB de almacenamiento o 4GB de memoria RAM con opciones de 64GB o 128GB de almacenamiento interno.', 2, 189, 'Redmi_Note_8T.jpg', 1),
 (12, 'Super_Mario_3D_World_Bowsers_Fury', '¡Únete a Mario, Luigi, Peach y Toad en su misión para salvar el reino de las hadas en Super Mario 3D World + Bowser’s Fury para Nintendo Switch! Rescata a la princesa hada y a sus amigas, en solitario o con hasta 3 jugadores más, en esta versión mejorada de Super Mario 3D World. Y después, también en solitario o con un amigo, ayuda a Bowsy a devolver a su papi a la normalidad en una aventura totalmente nueva: ¡Bowsers Fury!', 5, 40, 'Super_Mario_3D_World.jpg', 1),
-(13, 'BenQ_ZOWIE_XL2411K_Monitor_24_pulgadas_FullHD_144Hz_DyAc', 'La nueva generación de monitores de PC para e-sports de la serie XL aumenta aún más la flexibilidad y precisión de ajuste, así como la comodidad de los jugadores, lo que les permite centrarse en su rendimiento en el juego, con 144hz una base mas pequeña y un ajuste fluido y flexible incluyendo la nueva tecnología DyAc', 3, 279, 'monitorbenq.jpg', 0),
+(13, 'BenQ_ZOWIE_XL2411K_Monitor', 'La nueva generación de monitores de PC para e-sports de la serie XL aumenta aún más la flexibilidad y precisión de ajuste, así como la comodidad de los jugadores, lo que les permite centrarse en su rendimiento en el juego, con 144hz una base mas pequeña y un ajuste fluido y flexible incluyendo la nueva tecnología DyAc', 3, 279, 'monitorbenq.jpg', 0),
 (14, 'Seagate_Expansion_2_4TB_USB_3', 'Las unidades portátiles Expansion de Seagate son compactas y perfectas para un estilo de vida activo. Añada más espacio de almacenamiento instantáneamente a su equipo, y lleve sus archivos de gran tamaño a cualquier parte con este disco duro portátil de 2.5\" y una capacidad de 4TB.', 5, 95, 'usb4tb.jpg', 1),
 (25, 'Television LG 32LM6370PLA 32 pulgadas LED FullHD', 'Televisor 32LM6370PLA de LG, su pantalla Full HD ofrece imágenes precisas con una resolución extraordinaria y colores ricos. Estado bueno con un rasguño en la parte superior', 41, 278, 'televisionLG.jpg', 0);
 
@@ -101,7 +101,7 @@ CREATE TABLE `comentariossegundamano` (
 
 CREATE TABLE `oferta` (
   `Numero` int(32) NOT NULL,
-  `Nombre` varchar(32) COLLATE utf8_spanish_ci NOT NULL,
+  `Nombre` varchar(52) COLLATE utf8_spanish_ci NOT NULL,
   `Descripcion` varchar(1024) COLLATE utf8_spanish_ci NOT NULL,
   `URL_Oferta` varchar(512) COLLATE utf8_spanish_ci NOT NULL,
   `URL_Imagen` varchar(512) COLLATE utf8_spanish_ci NOT NULL,
@@ -124,7 +124,7 @@ INSERT INTO `oferta` (`Numero`, `Nombre`, `Descripcion`, `URL_Oferta`, `URL_Imag
 (2451, 'Silla_gaming', 'La silla de diseño Racing, Stinger Station Alien está creada para que disfrutes al máximo de una experiencia envolvente y confortable en tus largas e intensas sesiones de juegos y también en tus jornadas de trabajo o estudios. Podrás disfrutar de un diseño deportivo y ergonómico, fabricado en materiales de máxima calidad, muy duraderos, suaves y acolchados que duplican su confort. La serie ALIEN se define por su estética racing, su diseño \"de otra galaxia\" y su máxima calidad en la configuración de sus materiales.', 'https://www.pccomponentes.com/woxter-stinger-station-alien-silla-gaming-blue', 'sillagaming.jpg', 211, 109, 'persona@gmail.com', 0, 0),
 (2452, 'Logitech_Stereo_Speakers_Z120', 'Te presentamos los Logitech Stereo Speakers Z120, unos compactos altavoces USB con controles de volumen y encendido integrados facilitan la conexión de casi cualquier fuente de audio. Para que pueda disfrutar fácilmente de la música, el vídeo, etc. que prefiera. Estos altavoces compactos y versátiles son fáciles de conectar y controlar.', 'https://www.pccomponentes.com/logitech-stereo-speakers-z120', 'altavoceslogitech.jpg', 158, 15, 'persona@gmail.com', 0, 0),
 (2453, 'Logitech_M705_Raton_Inalambrico', 'El ratón Logitech Marathon Mouse M705 dura... y dura... y dura. Usa mucha menos energía que los ratones inalámbricos comparables de otras empresas, por lo que pueden pasar hasta tres años sin tener que cambiar las pilas. Además, cuenta con un receptor inalámbrico minúsculo que está acoplado permanentemente al ordenador, para que el ratón siempre esté listo para funcionar, en cualquier momento, en cualquier lugar y durante el tiempo necesario. Asimismo, el desplazamiento superrápido te permitirá desplazarte a gran velocidad por documentos largos para buscar lo que necesites sin pérdida de tiempo. El seguimiento láser ofrece un control preciso y uniforme del cursor. Y el diseño contorneado del ratón para la mano derecha resulta cómodo de principio a fin de la sesión.', 'https://www.pccomponentes.com/logitech-m705-marathon-mouse-raton-inalambrico-1000-dpi', 'raton.jpg', 80, 38, 'persona@gmail.com', 0, 0),
-(2454, 'Television LG 32LM6370PLA 32 pul', 'Nuevo televisor 32LM6370PLA de LG, su pantalla Full HD ofrece imágenes precisas con una resolución extraordinaria y colores ricos.', 'https://www.pccomponentes.com/lg-32lm6370pla-32-led-fullhd-hdr10', 'televisionLG.jpg', 41, 278, 'persona@gmail.com', 0, 1);
+(2454, 'Television LG 32LM6370PLA 32 pulgadas LED FullHD', 'Nuevo televisor 32LM6370PLA de LG, su pantalla Full HD ofrece imágenes precisas con una resolución extraordinaria y colores ricos.', 'https://www.pccomponentes.com/lg-32lm6370pla-32-led-fullhd-hdr10', 'televisionLG.jpg', 41, 278, 'persona@gmail.com', 0, 1);
 
 -- --------------------------------------------------------
 
